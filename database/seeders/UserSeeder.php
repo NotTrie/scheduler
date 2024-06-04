b5b6f240caf7b4dd925c69d15cd59048b07952b0
presidenwashil <raihanpresiden2003@gmail.com>
2024-06-04 16:17:57 +0800

    Seed initial user data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,10 +16,28 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::insert([
+            ['name' => 'Erdi', 'email' => 'erdi@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
             ['name' => 'Zidan', 'email' => 'zidan@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
             ['name' => 'Febroy', 'email' => 'febroy@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
             ['name' => 'Rizky', 'email' => 'rizky@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
-            ['name' => 'Raihan', 'email' => 'raihan@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)], 
+            ['name' => 'Andini', 'email' => 'andini@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Eunike', 'email' => 'eunike@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Dea', 'email' => 'dea@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Sipa', 'email' => 'sipa@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Filio', 'email' => 'filio@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Bayu', 'email' => 'bayu@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Firza', 'email' => 'firza@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Hafri', 'email' => 'hafri@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Ragil', 'email' => 'ragil@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Seka', 'email' => 'seka@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Michael', 'email' => 'michael@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Febry', 'email' => 'febry@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Wiliam', 'email' => 'wiliam@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'David', 'email' => 'david@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Fabian', 'email' => 'fabian@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Nouval', 'email' => 'nouval@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Faidil', 'email' => 'faidil@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
+            ['name' => 'Resifa', 'email' => 'resifa@gmail.com', 'email_verified_at' => now(), 'password' => Hash::make('password'), 'remember_token' => Str::random(10)],
         ]);
     }
 }
