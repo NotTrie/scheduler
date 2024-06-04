fd60a50882fd3b84ce7a6e931dcd4744d7c7106e
presidenwashil <raihanpresiden2003@gmail.com>
2024-06-04 16:30:46 +0800

    initial assistant

--- database/seeders/AssistantSeeder.php
+++ database/seeders/AssistantSeeder.php
@@ -14,10 +14,28 @@ class AssistantSeeder extends Seeder
     public function run(): void
     {
         Assistant::insert([
-            ['code' => 'ZD', 'name' => 'Zidan', 'user_id' => 2],
-            ['code' => 'TA', 'name' => 'Febroy', 'user_id' => 3],
-            ['code' => 'RZ', 'name' => 'Rizky', 'user_id' => 4],
-            ['code' => 'RN', 'name' => 'Raihan', 'user_id' => 5],
+            ['code' => 'ER','name' => 'Erdi', 'user_id' => 2], 
+            ['code' => 'ZD','name' => 'Zidan', 'user_id' => 3],
+            ['code' => 'TA','name' => 'Febroy', 'user_id' => 4],
+            ['code' => 'RZ','name' => 'Rizky', 'user_id' => 5],
+            ['code' => 'AD','name' => 'Andini', 'user_id' => 6],
+            ['code' => 'NK','name' => 'Eunike', 'user_id' => 7],
+            ['code' => 'DY','name' => 'Dea', 'user_id' => 8],
+            ['code' => 'SP','name' => 'Sipa', 'user_id' => 9],
+            ['code' => 'FO','name' => 'Filio', 'user_id' => 10],
+            ['code' => 'BY','name' => 'Bayu', 'user_id' => 11],
+            ['code' => 'FZ','name' => 'Firza', 'user_id' => 12],
+            ['code' => 'HF','name' => 'Hafri', 'user_id' => 13],
+            ['code' => 'RG','name' => 'Ragil', 'user_id' => 14],
+            ['code' => 'SK','name' => 'Seka', 'user_id' => 15],
+            ['code' => 'MC','name' => 'Michael', 'user_id' => 16],
+            ['code' => 'FI','name' => 'Febry', 'user_id' => 17],
+            ['code' => 'WC','name' => 'Wiliam', 'user_id' => 18],
+            ['code' => 'DV','name' => 'David', 'user_id' => 19],
+            ['code' => 'MF','name' => 'Fabian', 'user_id' => 20],
+            ['code' => 'NZ','name' => 'Nouval', 'user_id' => 21],
+            ['code' => 'FD','name' => 'Faidil', 'user_id' => 22],
+            ['code' => 'RA','name' => 'Resifa', 'user_id' => 23],
         ]);
     }
 }
