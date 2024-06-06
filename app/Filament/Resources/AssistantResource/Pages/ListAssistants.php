85e96b5f9b4beccbf7d945b5aead970b9884f6c2
presidenwashil <raihanpresiden2003@gmail.com>
2024-06-06 08:39:01 +0800

    chore: Enable registration role policy in Filament Shield configuration

--- app/Filament/Resources/AssistantResource/Pages/ListAssistants.php
+++ app/Filament/Resources/AssistantResource/Pages/ListAssistants.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\AssistantResource\Pages;
 
+use App\Models\Assistant;
 use App\Filament\Resources\AssistantResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListAssistants extends ListRecords
 {
@@ -15,5 +17,16 @@ class ListAssistants extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        if (auth()->user()->hasRole('super_admin')) {
+            return Assistant::query();
+        } else {
+            return Assistant::select('assistants.*')
+                ->join('users', 'assistants.user_id', '=', 'users.id')
+                ->where('users.id', auth()->id());
+        }
     }
 }
